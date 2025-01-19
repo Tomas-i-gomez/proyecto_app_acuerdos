287367c377d0afc0be911bac767e6b1761f709df
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-01-19 16:36:06 -0300

    refactor of the POST method in api/client

--- app/api/client/route.ts
+++ app/api/client/route.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({});
 
-
 //----------------------------------------------- MODELO CLIENTES ----------------------------------------------------------
 export async function GET() {
   const clientes = await prisma.clientes.findMany({
@@ -16,31 +15,38 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest) {
-    try {
-      const { id, razon_social, ramo } = await req.json();
-  
-      if (!id || !razon_social || !ramo) {
-        return NextResponse.json(
-          { error: "Todos los campos (Numero Cliente, nombre, telefono, ramo) son requeridos" },
-          { status: 400 }
-        );
-      }
-      // Crear el cliente en la base de datos
-      const nuevoCliente = await prisma.clientes.create({
-        data: {
-          id,
-          razon_social,
-          ramo,
-        },
-      });
-  
-      // Devolver la respuesta
-      return NextResponse.json(nuevoCliente, { status: 201 });
-    } catch (error) {
-      console.error("Error al crear cliente:", error);
+  try {
+    const { razon_social, ramoId } = await req.json();
+
+    // Validar que los campos obligatorios estén presentes y en el formato correcto
+    if (!razon_social || !ramoId || isNaN(Number(ramoId))) {
       return NextResponse.json(
-        { error: "Error interno del servidor" },
-        { status: 500 }
+        {
+          error:
+            "Los campos 'razon_social' y 'ramoId' son requeridos y deben ser válidos.",
+        },
+        { status: 400 }
       );
     }
+
+    // Crear el cliente en la base de datos
+    const nuevoCliente = await prisma.clientes.create({
+      data: {
+        razon_social,
+        ramoId: Number(ramoId), // Asegurarse de que sea un número
+      },
+      include: {
+        ramo: true,
+      },
+    });
+
+    // Devolver la respuesta con el nuevo cliente creado
+    return NextResponse.json(nuevoCliente, { status: 201 });
+  } catch (error) {
+    console.error("Error al crear cliente:", error);
+    return NextResponse.json(
+      { error: "Error interno del servidor" },
+      { status: 500 }
+    );
   }
+}
